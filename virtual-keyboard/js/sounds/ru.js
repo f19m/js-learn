95429cbe38e9c7232f324bc25753b8c599eb786b
f19m <leomelik@gmail.com>
2020-10-29 12:03:51 +0500

    init: add files to sounds effect

---
+++ virtual-keyboard/js/sounds/ru.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    url: '',
+    code: 'ShiftLeft',
+  },
+  {
+    url: '',
+    code: 'ShiftRight',
+  },
+  {
+    url: '',
+    code: 'CapsLock',
+  },
+  {
+    url: '',
+    code: 'Backspace',
+  },
+  {
+    url: '',
+    code: 'Enter',
+];
