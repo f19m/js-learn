1eadf276555f1483f00be1cc84f3306bd7218a4a
f19m <leomelik@gmail.com>
2020-11-13 16:31:58 +0500

    feat: add webpack

--- gem-puzzle/.eslintrc.js
+++ gem-puzzle/.eslintrc.js
@@ -1,17 +1,34 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es6: true,
   },
   extends: [
-    'airbnb-base',
+    'plugin:react/recommended',
+    'standard',
   ],
-  parser: 'babel-eslint',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: '16.12.0',
+    },
+  },
+  plugins: [
+    'react',
+  ],
   rules: {
-    'linebreak-style': ['error', 'windows'],
+    // disable rule of space before function parentheses
+    // "space-before-function-paren": 0
   },
 };
--- gem-puzzle/src/js/script.js
+++ gem-puzzle/src/js/script.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/extensions */
+import '../css/style.sass'
 import Puzzle from './Puzzle.js';
+
 
 document.addEventListener('DOMContentLoaded', () => {
   // eslint-disable-next-line no-unused-vars
