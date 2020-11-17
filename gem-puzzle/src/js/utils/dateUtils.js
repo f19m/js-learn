d81f16abcf542bf0880b6d3a08f1ec63b30be4d5
f19m <leomelik@gmail.com>
2020-11-17 12:15:27 +0500

    fix: eslint fix

--- gem-puzzle/src/js/utils/dateUtils.js
+++ gem-puzzle/src/js/utils/dateUtils.js
@@ -1,28 +1,24 @@
-
-
-export default function dateInit(){
-
-
-Date.prototype.yyyymmdd = function() {
-    var yyyy = this.getFullYear();
-    var mm = this.getMonth() < 9 ? "0" + (this.getMonth() + 1) : (this.getMonth() + 1); // getMonth() is zero-based
-    var dd = this.getDate() < 10 ? "0" + this.getDate() : this.getDate();
+/* eslint-disable no-extend-native */
+export default function dateInit() {
+  Date.prototype.yyyymmdd = () => {
+    const yyyy = this.getFullYear();
+    const mm = this.getMonth() < 9 ? `0${this.getMonth() + 1}` : (this.getMonth() + 1); // getMonth() is zero-based
+    const dd = this.getDate() < 10 ? `0${this.getDate()}` : this.getDate();
     return [yyyy, mm, dd].join('.');
-                
   };
-  
-  Date.prototype.yyyymmddhhmm = function() {
-    var yyyymmdd = this.yyyymmdd();
-    var hh = this.getHours() < 10 ? "0" + this.getHours() : this.getHours();
-    var min = this.getMinutes() < 10 ? "0" + this.getMinutes() : this.getMinutes();
-    return [yyyymmdd 
-            ,[hh, min].join(':')
-           ].join(' ');
+
+  Date.prototype.yyyymmddhhmm = () => {
+    const yyyymmdd = this.yyyymmdd();
+    const hh = this.getHours() < 10 ? `0${this.getHours()}` : this.getHours();
+    const min = this.getMinutes() < 10 ? `0${this.getMinutes()}` : this.getMinutes();
+    return [yyyymmdd,
+      [hh, min].join(':'),
+    ].join(' ');
   };
-  
-  Date.prototype.yyyymmddhhmmss = function() {
-    var yyyymmddhhmm = this.yyyymmddhhmm();
-    var ss = this.getSeconds() < 10 ? "0" + this.getSeconds() : this.getSeconds();
+
+  Date.prototype.yyyymmddhhmmss = () => {
+    const yyyymmddhhmm = this.yyyymmddhhmm();
+    const ss = this.getSeconds() < 10 ? `0${this.getSeconds()}` : this.getSeconds();
     return [yyyymmddhhmm, ss].join(':');
   };
 }
