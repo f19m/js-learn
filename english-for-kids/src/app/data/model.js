6ef6355c5e88e4e7310e4c9c4da7655045f47959
f19m <leomelik@gmail.com>
2020-11-20 13:33:52 +0500

    init: add img

--- english-for-kids/src/app/data/model.js
+++ english-for-kids/src/app/data/model.js
@@ -3,131 +3,282 @@ export default {
   Pages: {
     categories: [
       {
-        Name: 'Food',
-        img: '',
+        Name: 'Animals',
+        img: './assets/images/main/animals.jpg',
         words: [
           {
-            name: 'Apple',
-            translate: 'Яблоко',
-            url: '',
-            audio: '',
-
+            name: 'Alpaca', translate: 'Альпака', img: './assets/images/main/animals/alpaca.jpg', audio: '',
           },
           {
-            name: 'Banana',
-            translate: 'Банан',
-            url: '',
-            audio: '',
-
+            name: 'Cat', translate: 'Кот', img: './assets/images/main/animals/cat.jpg', audio: '',
           },
           {
-            name: 'Рыба',
-            translate: 'Fish',
-            url: '',
-            audio: '',
-
+            name: 'Fox', translate: 'Лиса', img: './assets/images/main/animals/fox.jpg', audio: '',
           },
           {
-            name: 'Cheese',
-            translate: 'Сыр',
-            url: '',
-            audio: '',
-
+            name: 'Hedgehog', translate: 'Ёжик', img: './assets/images/main/animals/hedgehog.jpg', audio: '',
           },
           {
-            name: 'Bread',
-            translate: 'Хлеб',
-            url: '',
-            audio: '',
-
+            name: 'Koala', translate: 'Коала', img: './assets/images/main/animals/koala.jpg', audio: '',
           },
           {
-            name: 'Tomato',
-            translate: 'Помидор',
-            url: '',
-            audio: '',
-
+            name: 'Panda', translate: 'Панда', img: './assets/images/main/animals/panda.jpg', audio: '',
           },
           {
-            name: 'Shrimp',
-            translate: 'Креветка',
-            url: '',
-            audio: '',
-
+            name: 'Tiger', translate: 'Тигр', img: './assets/images/main/animals/tiger.jpg', audio: '',
           },
           {
-            name: 'Cabbage',
-            translate: 'Капуста',
-            url: '',
-            audio: '',
-
+            name: 'Whale', translate: 'Кит', img: './assets/images/main/animals/whale.jpg', audio: '',
           },
         ],
-
       },
 
       {
-        Name: 'Animals',
-        img: '',
+        Name: 'Animals II',
+        img: './assets/images/main/animals2.jpg',
         words: [
           {
-            name: 'Pig',
-            translate: 'Свинья',
-            url: '',
-            audio: '',
-
+            name: 'Beaver', translate: 'Бобр', img: './assets/images/main/animals2/beaver.jpg', audio: '',
           },
           {
-            name: 'Fox',
-            translate: 'Лиса',
-            url: '',
-            audio: '',
-
+            name: 'Elephant', translate: 'Слон', img: './assets/images/main/animals2/elephant.jpg', audio: '',
           },
           {
-            name: 'Koala',
-            translate: 'Коала',
-            url: '',
-            audio: '',
-
+            name: 'Giraffe', translate: 'Жираф', img: './assets/images/main/animals2/giraffe.jpg', audio: '',
           },
           {
-            name: 'Tiger',
-            translate: 'Тигр',
-            url: '',
-            audio: '',
-
+            name: 'Lion', translate: 'Лев', img: './assets/images/main/animals2/lion.jpg', audio: '',
           },
           {
-            name: 'Zebra',
-            translate: 'Зебра',
-            url: '',
-            audio: '',
-
+            name: 'Mouse', translate: 'Мышь', img: './assets/images/main/animals2/mouse.jpg', audio: '',
           },
           {
-            name: 'Bull',
-            translate: 'Бык',
-            url: '',
-            audio: '',
-
+            name: 'Parrot', translate: 'Папугай', img: './assets/images/main/animals2/parrot.jpg', audio: '',
           },
           {
-            name: 'Hedgehog',
-            translate: 'Ёжик',
-            url: '',
-            audio: '',
-
+            name: 'Penguin', translate: 'Пингвин', img: './assets/images/main/animals2/penguin.jpg', audio: '',
           },
           {
-            name: 'Whale',
-            translate: 'Кит',
-            url: '',
-            audio: '',
-
+            name: 'Rhino', translate: 'Носорог', img: './assets/images/main/animals2/rhino.jpg', audio: '',
           },
         ],
+      },
 
+      {
+        Name: 'Food',
+        img: './assets/images/main/food.jpg',
+        words: [
+          {
+            name: 'Donuts', translate: 'Пончик', img: './assets/images/main/food/donuts.jpg', audio: '',
+          },
+          {
+            name: 'Hamburger', translate: 'Гамбургер', img: './assets/images/main/food/hamburger.jpg', audio: '',
+          },
+          {
+            name: 'Hotdog', translate: 'Хотдог', img: './assets/images/main/food/hotdog.jpg', audio: '',
+          },
+          {
+            name: 'Icecream', translate: 'Мороженое', img: './assets/images/main/food/icecream.jpg', audio: '',
+          },
+          {
+            name: 'Pizza', translate: 'Пицца', img: './assets/images/main/food/pizza.jpg', audio: '',
+          },
+          {
+            name: 'Ramen', translate: 'Рамен', img: './assets/images/main/food/ramen.jpg', audio: '',
+          },
+          {
+            name: 'Sandwich', translate: 'Сэндвич', img: './assets/images/main/food/sandwich.jpg', audio: '',
+          },
+          {
+            name: 'Taco', translate: 'Тако', img: './assets/images/main/food/taco.jpg', audio: '',
+          },
+        ],
+      },
+
+      {
+        Name: 'fruit',
+        img: './assets/images/main/fruit.jpg',
+        words: [
+          {
+            name: 'Avocado', translate: 'Авокадо', img: './assets/images/main/fruit/avocado.jpg', audio: '',
+          },
+          {
+            name: 'Banana', translate: 'Банан', img: './assets/images/main/fruit/banana.jpg', audio: '',
+          },
+          {
+            name: 'Kiwi', translate: 'Киви', img: './assets/images/main/fruit/kiwi.jpg', audio: '',
+          },
+          {
+            name: 'Lemon', translate: 'Лемон', img: './assets/images/main/fruit/lemon.jpg', audio: '',
+          },
+          {
+            name: 'Orange', translate: 'Апельсин', img: './assets/images/main/fruit/orange.jpg', audio: '',
+          },
+          {
+            name: 'Pineapple', translate: 'Ананас', img: './assets/images/main/fruit/pineapple.jpg', audio: '',
+          },
+          {
+            name: 'Strawberry', translate: 'Клубника', img: './assets/images/main/fruit/strawberry.jpg', audio: '',
+          },
+          {
+            name: 'Watermelon', translate: 'Арбуз', img: './assets/images/main/fruit/watermelon.jpg', audio: '',
+          },
+        ],
+      },
+
+      {
+        Name: 'job',
+        img: './assets/images/main/job.jpg',
+        words: [
+          {
+            name: 'Chef', translate: 'Шеф-повар', img: './assets/images/main/job/chef.jpg', audio: '',
+          },
+          {
+            name: 'Doctor', translate: 'Доктор', img: './assets/images/main/job/doctor.jpg', audio: '',
+          },
+          {
+            name: 'Farmer', translate: 'Фермер', img: './assets/images/main/job/farmer.jpg', audio: '',
+          },
+          {
+            name: 'Firefighter', translate: 'Пожарный', img: './assets/images/main/job/firefighter.jpg', audio: '',
+          },
+          {
+            name: 'Police', translate: 'Полицейский', img: './assets/images/main/job/police.jpg', audio: '',
+          },
+          {
+            name: 'Stewardess', translate: 'Стюардесса', img: './assets/images/main/job/stewardess.jpg', audio: '',
+          },
+          {
+            name: 'Teacher', translate: 'Учитель', img: './assets/images/main/job/teacher.jpg', audio: '',
+          },
+          {
+            name: 'Veterinarian', translate: 'Ветеринар', img: './assets/images/main/job/veterinarian.jpg', audio: '',
+          },
+        ],
+      },
+
+      {
+        Name: 'music',
+        img: './assets/images/main/music.jpg',
+        words: [
+          {
+            name: 'Cello', translate: 'Виолончель', img: './assets/images/main/music/cello.jpg', audio: '',
+          },
+          {
+            name: 'Drum', translate: 'Барабан', img: './assets/images/main/music/drum.jpg', audio: '',
+          },
+          {
+            name: 'Guitar', translate: 'Гитара', img: './assets/images/main/music/guitar.jpg', audio: '',
+          },
+          {
+            name: 'Headphone', translate: 'Наушники', img: './assets/images/main/music/headphone.jpg', audio: '',
+          },
+          {
+            name: 'Maraca', translate: 'Маракасы', img: './assets/images/main/music/police.jpg', maraca: '',
+          },
+          {
+            name: 'Microphone', translate: 'Микрафон', img: './assets/images/main/music/microphone.jpg', audio: '',
+          },
+          {
+            name: 'Saxophone', translate: 'Саксафон', img: './assets/images/main/music/saxophone.jpg', audio: '',
+          },
+          {
+            name: 'Trumpet', translate: 'Труба', img: './assets/images/main/music/trumpet.jpg', audio: '',
+          },
+        ],
+      },
+
+      {
+        Name: 'places',
+        img: './assets/images/main/places.jpg',
+        words: [
+          {
+            name: 'Big Ben', translate: 'Биг Бэн', img: './assets/images/main/places/big-ben.jpg', audio: '',
+          },
+          {
+            name: 'Burj-khalifa', translate: 'Бурдж-Халифа', img: './assets/images/main/places/burj-khalifa.jpg', audio: '',
+          },
+          {
+            name: 'Colosseum', translate: 'Колизей', img: './assets/images/main/places/colosseum.jpg', audio: '',
+          },
+          {
+            name: 'La Sagrada Familia', translate: 'Саграда Фамилия', img: './assets/images/main/places/la-sagrada-familia.jpg', audio: '',
+          },
+          {
+            name: 'Liberty Statue', translate: 'Статуя свободы', img: './assets/images/main/places/liberty-statue.jpg', maraca: '',
+          },
+          {
+            name: 'Pyramid', translate: 'Пирамида', img: './assets/images/main/places/pyramid.jpg', audio: '',
+          },
+          {
+            name: 'Sydney opera house', translate: 'Сиднейский оперный театр', img: './assets/images/main/places/sydney-opera-house.jpg', audio: '',
+          },
+          {
+            name: 'The Leaning Tower of Pisa', translate: 'Труба', img: './assets/images/main/places/tower-pisa.jpg', audio: '',
+          },
+        ],
+      },
+
+      {
+        Name: 'space',
+        img: './assets/images/main/space.jpg',
+        words: [
+          {
+            name: 'Alien', translate: 'Пришелец', img: './assets/images/main/space/alien.jpg', audio: '',
+          },
+          {
+            name: 'Astronaut', translate: 'Астронавт', img: './assets/images/main/space/astronaut.jpg', audio: '',
+          },
+          {
+            name: 'Earth', translate: 'Земля', img: './assets/images/main/space/earth.jpg', audio: '',
+          },
+          {
+            name: 'Moon', translate: 'Луна', img: './assets/images/main/space/moon.jpg', audio: '',
+          },
+          {
+            name: 'Rocket', translate: 'Ракета', img: './assets/images/main/space/rocket.jpg', maraca: '',
+          },
+          {
+            name: 'Satellite', translate: 'Спутник', img: './assets/images/main/space/space-satellite.jpg', audio: '',
+          },
+          {
+            name: 'Space Shuttle', translate: 'Космический Шатл', img: './assets/images/main/space/space-shuttle.jpg', audio: '',
+          },
+          {
+            name: 'Telescope', translate: 'Телескоп', img: './assets/images/main/space/telescope.jpg', audio: '',
+          },
+        ],
+      },
+
+      {
+        Name: 'sport',
+        img: './assets/images/main/sport.jpg',
+        words: [
+          {
+            name: 'Baseball', translate: 'Бейсбол', img: './assets/images/main/sport/baseball.jpg', audio: '',
+          },
+          {
+            name: 'Basketball', translate: 'Баскетбол', img: './assets/images/main/sport/basketball.jpg', audio: '',
+          },
+          {
+            name: 'Bowling', translate: 'Боулинг', img: './assets/images/main/sport/bowling.jpg', audio: '',
+          },
+          {
+            name: 'Football', translate: 'Футбол', img: './assets/images/main/sport/football.jpg', audio: '',
+          },
+          {
+            name: 'Rugby', translate: 'Регби', img: './assets/images/main/sport/rugby.jpg', maraca: '',
+          },
+          {
+            name: 'Table Tennis', translate: 'Настольный Теннис', img: './assets/images/main/sport/table-tennis.jpg', audio: '',
+          },
+          {
+            name: 'Tennis', translate: 'Теннис', img: './assets/images/main/sport/tennis.jpg', audio: '',
+          },
+          {
+            name: 'Volleyball', translate: 'Волейбол', img: './assets/images/main/sport/volley.jpg', audio: '',
+          },
+        ],
       },
 
     ],
